e4210bfc3738cc0019e817c3e898f6d777a7007c
strivegfx <notifydevon@gmail.com>
2017-12-21 16:43:12 +1300

    feat: stage cors origin
    allow state.enhancedigital as part of the dynamic cors Access-Control-Allow-Origin response header
    system

--- src/utils/cors.ts
+++ src/utils/cors.ts
@@ -1,14 +1,43 @@
 import { APIGatewayEvent } from '../../node_modules/@types/aws-lambda/';
-import { origin as requiredOrigin } from './config';
+import { isProduction, origin as requiredOrigin } from './config';
+
+const querySubdomain = (suppliedOrigin: string) => {
+  const subStage = '://stage.';
+  const subWww = '://www.';
+  const subDefault = '://';
+
+  return [subStage, subWww].reduce((acc, subdomain) => {
+    const isMatch = suppliedOrigin.includes(subdomain);
+
+    return isMatch ? subdomain : acc;
+  }, subDefault);
+};
+
+// Because "Access-Control-Allow-Origin" only accepts a single value and we have
+// multiple origins that we need to support...
+//
+// + stage.enhancedigital.co.nz
+// + www.enhancedigital.co.nz
+// + enhancedigital.co.nz
+//
+// We need to build up the correct suported origin based on the supplied origin
+// values. This means that we query the supplied origin and build a custom Enhance
+// Digital specific origin that reflects the supplied versions properties (HTTP / HTTPS)
+// or what subdomain is referenced.
+const createAllowedOrigin = (suppliedOrigin: string) => {
+  const protocol = suppliedOrigin.startsWith('https') ? 'https' : 'http';
+  const subdomain = querySubdomain(suppliedOrigin);
+
+  return `${protocol}${subdomain}${requiredOrigin}`;
+};
 
 // The required CORS headers when generating a response POST request.
 const createHeaders = (suppliedOrigin: string) => {
-  const isWww = suppliedOrigin.includes('//www.');
-  const prepend = isWww ? 'https://www.' : 'https://';
+  const allowedOrigin = createAllowedOrigin(suppliedOrigin);
 
   return {
     // Required for CORS support to work
-    'Access-Control-Allow-Origin': `${prepend}${requiredOrigin}`,
+    'Access-Control-Allow-Origin': allowedOrigin,
     // Required for cookies, authorization headers with HTTPS
     'Access-Control-Allow-Credentials': true,
   };
--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,51 @@
 const path = require('path');
-const {DefinePlugin} = require('webpack');
+const { DefinePlugin } = require('webpack');
 
-const {
-  AWS_ACCESS_KEY_ID = '',
-  AWS_SECRET_ACCESS_KEY = ''
-} = process.env;
+const { AWS_ACCESS_KEY_ID = '', AWS_SECRET_ACCESS_KEY = '' } = process.env;
 
 module.exports = () => ({
-
   entry: './src/',
 
   output: {
-
     path: path.resolve(__dirname),
 
     filename: 'handler.js',
 
-    libraryTarget: 'commonjs'
-
+    libraryTarget: 'commonjs',
   },
 
   module: {
-  
     rules: [
-
       {
         test: /\.ts$/,
         include: [path.resolve(__dirname, 'src')],
         enforce: 'pre',
-        loader: 'tslint-loader'
+        loader: 'tslint-loader',
       },
 
       {
         test: /\.ts$/,
         include: [path.resolve(__dirname, 'src')],
-        use: [{ loader: 'ts-loader' }]
-      }
-    
-    ]
-
+        use: [{ loader: 'ts-loader' }],
+      },
+    ],
   },
 
   resolve: {
-    
-    extensions: [ '.tsx', '.ts', '.js' ]
-  
+    extensions: ['.tsx', '.ts', '.js'],
   },
 
   plugins: [
-
     new DefinePlugin({
       AWS_ACCESS_KEY_ID: JSON.stringify(AWS_ACCESS_KEY_ID),
-      AWS_SECRET_ACCESS_KEY: JSON.stringify(AWS_SECRET_ACCESS_KEY)
-    })
-
+      AWS_SECRET_ACCESS_KEY: JSON.stringify(AWS_SECRET_ACCESS_KEY),
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
+    }),
   ],
 
   devtool: false, // 'inline-source-map',
 
   target: 'node',
 
-  context: __dirname
-
+  context: __dirname,
 });
